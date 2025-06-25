48684b33a1f57ebf3bd64a82ae730696129a940c
Sylvia <hibernatingfeline@gmail.com>
2025-06-25 19:46:45 +0700

    adding credentials

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Gumball Quiz',
+  description: 'With much love and less effort, Syl',
+  generator: 'me :)',
 }
 
 export default function RootLayout({
